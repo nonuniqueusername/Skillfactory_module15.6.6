5195084108650d026894ecb576f37d2d3a3af64e
s.ovsyannikov <s.ovsyannikov@tele2.ru>
2023-07-28 17:19:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Skillfactory_module15._6._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes.SelectMany(x => x.Students).ToArray();
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
